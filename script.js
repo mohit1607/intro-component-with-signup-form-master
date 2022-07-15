23884536a2fd156dc699b38d136f6c2cf88acec3
Mohit Ashiliya <mohit.ashliya@gmail.com>
2022-07-15 21:49:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const allInputs = document.querySelectorAll('.input')
+
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', () => {
+    allInputs.firstChild.forEach((curr) => {
+        if(curr.textContent === '') {
+           
+        } else {
+            
+        }
+    })
+})
